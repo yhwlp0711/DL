ca5e41ac94548c4d4d880febaca5a339ab7f21ae
Kasumi <yhwlp0711@gmail.com>
2024-11-15 18:53:11 +0800

    2024.11.15 from Windows

--- nlp/Seq2Seq/Seq2Seq.py
+++ nlp/Seq2Seq/Seq2Seq.py
@@ -201,17 +201,23 @@ def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps,
 
 def bleu(pred_seq, label_seq, k):
     """计算 BLEU"""
+    # 将预测序列和标签序列分词
     pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
     len_pred, len_label = len(pred_tokens), len(label_tokens)
+    # 计算BLEU的初始分数，公式为：exp(min(0, 1 - len_label / len_pred))
     score = math.exp(min(0, 1 - len_label / len_pred))
     for n in range(1, k + 1):
         num_matches, label_subs = 0, collections.defaultdict(int)
+        # 统计标签序列中长度为n的子序列的频数
         for i in range(len_label - n + 1):
             label_subs[''.join(label_tokens[i:i + n])] += 1
+        # 统计预测序列中与标签序列匹配的长度为n的子序列的个数
         for i in range(len_pred - n + 1):
             if label_subs[''.join(pred_tokens[i:i + n])] > 0:
                 num_matches += 1
                 label_subs[''.join(pred_tokens[i:i + n])] -= 1
+        # 计算n-gram的精度，公式为：num_matches / (len_pred - n + 1)
+        # 计算BLEU分数，公式为：score *= (num_matches / (len_pred - n + 1)) ** (0.5 ** n)
         score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
     return score
 
